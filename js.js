8d170d5629e7dce243f43681e3f271e2af042b89
Kate_Kalashnikova <68355941+KateKalashnikova@users.noreply.github.com>
2021-04-17 21:34:19 -0600

    added score variable

--- js.js
+++ js.js
@@ -1,3 +1,12 @@
+var player = {
+	x:50,
+	y:100,
+	pacmouth:320,
+	pacdir:0,
+}
+var score = 0;
+var gscore = 0;
+
 var canvas = document.createElement('canvas');
 var context = canvas.getContext("2d");
 canvas.height = 400;
@@ -19,7 +28,10 @@ function playGame() {
 function render() {
 	context.fillStyle = '#050A50';
 	context.fillRect(0,0,canvas.width, canvas.height);
-	context.drawImage(mainImage,320,0,32,32,50,50,35,35);
+	context.drawImage(mainImage,player.pacmouth,player.pacdir,32,32,player.x,player.y,32,32);
+	context.font = '20px Verdana';
+	context.fillStyle = 'white';
+	context.fillText("Pacman: "+score+" vs Ghost:"+gscore,175,18);
 }
 
 document.body.appendChild(canvas);
