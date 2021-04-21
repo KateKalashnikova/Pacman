eb549a8fbf1f7c33c0aa88ae73d5d262303206d9
Kate_Kalashnikova <68355941+KateKalashnikova@users.noreply.github.com>
2021-04-20 23:28:38 -0600

    random ghost

--- js.js
+++ js.js
@@ -13,8 +13,9 @@ var enemy = {
 	speed:5
 }
 
-var score = 0;
-var gscore = 0;
+var ghost = false;
+var score = 0, gscore = 0;
+
 
 var canvas = document.createElement('canvas');
 var context = canvas.getContext("2d");
@@ -38,32 +39,32 @@ document.addEventListener('keyup', function (event) {
 function move(keyclick) {
 	if (37 in keyclick)
 		{player.x -= player.speed;
-		player.pacdir=64;}
+		player.pacdir = 64;}
 	if (38 in keyclick)
 		{player.y -= player.speed;
-		player.pacdir=96;}
+		player.pacdir = 96;}
 	if (39 in keyclick)
 		{player.x += player.speed;
-		player.pacdir=0;}
+		player.pacdir = 0;}
 	if (40 in keyclick)
 		{player.y += player.speed;
-		player.pacdir=32;}
+		player.pacdir = 32;}
 	if(player.x >= (canvas.width-32)) {
-		player.x=0;
+		player.x = 0;
 	}	
 	if(player.y >= (canvas.height-32)) {
-		player.y=0;
+		player.y = 0;
 	}	
 	if(player.x < 0) {
 		player.x=(canvas.width-32);
 	}	
 	if(player.y < 0) {
-		player.y=(canvas.height-32);
+		player.y = (canvas.height-32);
 	}		
-	if(player.pacmouth==320){
-		player.pacmouth=352;
+	if(player.pacmouth == 320){
+		player.pacmouth = 352;
 	} else {
-		player.pacmouth=320;
+		player.pacmouth = 320;
 	}
 	render();
 }
@@ -79,15 +80,26 @@ function playGame() {
 	requestAnimationFrame(playGame);
 }
 
+function myNum(n) {
+	return Math.floor(Math.random()*n );
+}
+
 function render() {
 	context.fillStyle = '#050A50';
 	context.fillRect(0,0,canvas.width, canvas.height);
+
+	if(!ghost) {
+		enemy.ghostNum = myNum(5)*64;
+		enemy.x = myNum(450);
+		enemy.y = myNum(250)+30;
+		ghost = true;
+	}
 
     context.font = '20px Verdana';
 	context.fillStyle = 'white';
 	context.fillText("Pacman: "+score+" vs Ghost:"+gscore,175,18);
 
-	context.drawImage(mainImage,0,0,32,32,enemy.x,enemy.y,32,32);
+	context.drawImage(mainImage,enemy.ghostNum,0,32,32,enemy.x,enemy.y,32,32);
 	context.drawImage(mainImage,player.pacmouth,player.pacdir,32,32,player.x,player.y,32,32);
 	
 }
